14f416946d53e63f5911765e2970f667ccfa1fc6
Michał Martyniuk <michal.martyniuk7@gmail.com>
2019-11-06 13:15:19 +0100

    merge conflict resolved

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import '@fortawesome/fontawesome-free/css/all.min.css';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-import './index.css';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { rootReducer } from './store/rootReducer';
-=======
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
@@ -28,21 +12,14 @@ import { createStore, applyMiddleware } from "redux";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import rootReducer from "./store/rootReducer";
->>>>>>> 40fdedf823d3a2ccc898fdce1f189dbbfbd74221
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-<<<<<<< HEAD
-  // eslint-disable-next-line react/jsx-filename-extension
-  <Provider store={store}><App /></Provider>,
-  document.getElementById('root')
-=======
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
->>>>>>> 40fdedf823d3a2ccc898fdce1f189dbbfbd74221
 );
 
 // If you want your app to work offline and load faster, you can change
